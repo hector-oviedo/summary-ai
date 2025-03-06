9e7243bcebaea9f04c9e4d0d2d3f53b35563ebb1
hector <hector.oviedo@iu-study.org>
2025-03-06 06:23:57 -0300

    added frontend working, added docker and docker compose system, added screenshots, updated readme

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes import api
 from app.config import Config
 from app.utils.logger import get_logger
@@ -8,6 +9,8 @@ logger = get_logger(__name__)
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    # Enable CORS to allow requests from http://localhost:5173
+    CORS(app, origins=["http://localhost:5173"])
     app.register_blueprint(api, url_prefix="/api")
     return app
 
